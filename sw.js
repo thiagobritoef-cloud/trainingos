967082615b8fd893de6b6c48a0512a88e22d4b17
thiagobritoef-cloud <thiagobrito.ef@gmail.com>
2026-06-23 15:21:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,31 @@
+const CACHE_NAME = 'trainingos-v3'
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png']
+
+self.addEventListener('install', e => {
+  self.skipWaiting()
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).catch(() => {}))
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return
+  e.respondWith(
+    fetch(e.request).then(r => {
+      if (r.ok) {
+        const clone = r.clone()
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone))
+      }
+      return r
+    }).catch(() => caches.match(e.request))
+  )
+})
 const CACHE = 'trainingos-v1';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
