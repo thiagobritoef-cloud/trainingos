bc7617d9987aa3110500e839321282db6c49cf5b
thiagobritoef-cloud <thiagobrito.ef@gmail.com>
2026-06-25 20:55:41 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,48 +1,27 @@
-const CACHE_NAME = 'trainingos-v3'
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png']
-
-self.addEventListener('install', e => {
-  self.skipWaiting()
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).catch(() => {}))
-})
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  )
-})
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return
-  e.respondWith(
-    fetch(e.request).then(r => {
-      if (r.ok) {
-        const clone = r.clone()
-        caches.open(CACHE_NAME).then(c => c.put(e.request, clone))
-      }
-      return r
-    }).catch(() => caches.match(e.request))
-  )
-})
-const CACHE = 'trainingos-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+var V = 'tos-v6';
+self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) {
+        if (k !== V) return caches.delete(k);
+      }));
+    }).then(function() { return self.clients.claim(); })
+  );
 });
-
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', function(e) {
+  var url = e.request.url;
+  if (url.indexOf('supabase') >= 0 || url.indexOf('cdn.jsdelivr') >= 0) return;
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request).then(function(res) {
+      return caches.open(V).then(function(c) {
+        c.put(e.request, res.clone());
+        return res;
+      });
+    }).catch(function() {
+      return caches.match(e.request);
+    })
   );
 });
